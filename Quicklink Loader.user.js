fe1aa3564cd5230ccde2b5c494bb274ba9467501
NyeUsr <108840615+NyeUsr@users.noreply.github.com>
2023-03-03 15:20:27 -0600

    Various improvements.

--- Quicklink Loader.user.js
+++ Quicklink Loader.user.js
@@ -1,8 +1,11 @@
 // ==UserScript==
 // @name        Quicklink Loader
+// @icon        https://getquick.link/assets/images/icons/favicon-32x32.png
 // @match       *://*/*
+// @exclude     *://search.brave.com/*
+// @exclude     *://github.com/*
 // @grant       none
-// @version     1.1.2
+// @version     1.1.3
 // @author      NoUser
 // @description Quicklink is a JavaScript library that speeds up subsequent page loads by intelligently prefetching resources. This userscript adds Quicklink to every webpage, preloading links that the user is likely to click. By prefetching these resources, the next page loads faster, improving the overall browsing experience.
 // @namespace   https://greasyfork.org/en/scripts/459274-quicklink-loader
@@ -20,17 +23,20 @@ const ignore = [
   /^api\./,
   // Ignore login/signup urls
   /\/(sign|log)\/?/,
+  // Ignore "Premium" stuffs
+  /^https?:\/\/.+\/(.+)?premium/,
   // Ignore urls that contain the word "video"
   uri => uri.includes('video'),
   // Don't prefetch links with dom selectors
   uri => uri.includes('#'),
-  // Don't prefetch these file types
-  uri => ['.zip', '.tar', '.7z', '.rar', '.json', '.apk', '.xapk', '.woff2', '.tff', '.otf', '.pdf', '.mp3', '.mp4', '.wav', '.exe', '.msi', '.bat', '.deb', '.rpm', '.bin', '.dmg', '.iso', '.csv', '.log', '.sql', '.xml', '.key', '.odp', '.ods', '.pps', '.ppt', '.xls', '.doc'].some(ext => uri.includes(ext)),
+  // Don't prefetch socials
+  uri => ['youtube.com', 'youtu.be', 'youtube-nocookie.com', 'youtubeeducation.com', 'discord.com', 'discordapp.com', 'facebook.com', 'facebook.net', 'pin.it', 'pinimg.com', 'pinterest.com', 'redd.it', 'reddit.com', 'redditmedia.com', 'tiktok.com', 'twitter.com', 'twimg.com', 't.co'].some(soc => uri.includes(soc)),
+  // Don't prefetch these file types and extensions
+  uri => /\.(zip|tar|7z|rar|js|apk|xapk|woff2|tff|otf|pdf|mp3|mp4|wav|exe|msi|bat|deb|rpm|bin|dmg|iso|csv|log|sql|xml|key|odp|ods|pps|ppt|xls|doc|jpg|jpeg|jpe|jif|jfif|jfi|png|gif|webp|tif|psd|raw|arw|cr2|nrw|k25|bmp|dib|heif|heic|ind|indd|indt|jp2|j2k|jpf|jpx|jpm|mj2|svg|ai|eps)$/i.test(uri),
   // Don't prefetch these protocols
-  uri => ['http:', 'file:', 'ftp:', 'mailto:', 'tel:'].some(protocol => uri.includes(protocol)),
+  uri => /^(http|file|ftp|mailto|tel):/i.test(uri),
   // Ignore all links, scripts which has explicit noprefetch
   (uri, elem) => elem.hasAttribute('noprefetch'),
 ];
 
-
-quicklink.listen({ origins: true, limit: 15, ignores: ignore });
+quicklink.listen({ origins: true, limit: 15, delay: 2000, ignores: ignore });
